d16019e0cbffa04d01222a3254c7b01cabe4f86c
Mlara <64501664+Mlara73@users.noreply.github.com>
2020-10-13 22:21:24 -0500

    Fix Real Prediction with LinearRegression model and uploading Flask init version

---
+++ Flask/app.py
@@ -0,0 +1,26 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    array_features = [np.array(int_features)]
+    df_features = pd.DataFrame(array_features,columns=['make','model','year','mileage','transmission','fuelType','mpg','engineSize','tax'])
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Car Price should be $ {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
