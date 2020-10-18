db85f4085768c14645e1a8b20309319130ca3544
Anurag Awasthi <awasthia@Riversand.com>
2020-10-17 19:56:09 -0500

    final front end with tableau and logic on images

--- usedcars/app.py
+++ usedcars/app.py
@@ -10,38 +10,50 @@ app = Flask(__name__)
 # with open(f'best_logistic_reg_model.pickle', "rb") as f:
 filepath =  os.path.join("usedcars", "data", 'best_xgb_pipeline_model.pickle')
 
-with open(filepath, "rb") as f:
-    model = pickle.load(f)
+def loadmodel():
+
+    with open(filepath, "rb") as f:
+        model = pickle.load(f)
+    return(model)
 
 # feature_names = model.get_booster().feature_names
 
 # Route to render index.html template using data from Mongo
 @app.route("/", methods=["GET", "POST"])
-def home():
-    output_message = ""
+def home():            
+        
+    return render_template("index.html")
 
-    if request.method == "POST":
-        carmake = request.form["carmake"]
-        carmodel = request.form["carmodel"]
-        purchaseyear = int(request.form["purchaseyear"])
-        transmission = request.form["transmission"]
-        fueltype = request.form["fueltype"]
-        mileage = float(request.form["mileage"])
-        mpg = float(request.form["mpg"])
-        enginesize = float(request.form["enginesize"])
-        print(f'model is {carmake}')
-        
-        # data must be converted to df with matching feature names before predict
-        # data = pd.DataFrame(np.array([[recency, frequency, monetary, time]]), columns=feature_names)
-        testdata = np.array([[1.17]])
-        rawdata = pd.DataFrame(np.array([[carmodel,purchaseyear,transmission,mileage,fueltype,mpg,enginesize,carmake]]), columns=['model','year','transmission','mileage','fuelType','mpg','engineSize','make'])
-        
-        # result = model.predict(testdata)
-        # output_message = f'Please check results === {carmake} and ======{carmodel} ======== {purchaseyear} ======= {transmission}============={fueltype}============{mileage}======={mpg} ==== {enginesize}'
-        result = model.predict(rawdata)
-        output_message = f'Please check results =========={result}'
-        
-    return render_template("index.html", message = output_message)
+@app.route("/predict/<make>/<model>/<purchaseyear>/<transmission>/<fuelType>/<mileage>/<mpg>/<engineSize>")
+def predictInput(make, model,purchaseyear,transmission, fuelType, mileage,mpg,engineSize):
+    output_message = ""
+    print("Hello")
+    if  mileage.isnumeric():
+        print("test")
+        try:
+            mpg_value = float(mpg)
+            print(mpg_value)
+            # carmake = request.form["carmake"]
+            # carmodel = request.form["carmodel"]
+            purchaseyear_value = int(purchaseyear)
+            print(purchaseyear_value)
+            # transmission = request.form["transmission"]
+            # fueltype = request.form["fueltype"]
+            mileage_value = float(mileage)
+            print(mileage_value)
+            enginesize_value = float(engineSize)
+            print(enginesize_value)
+            # print(f'model is {carmake}')
+                    
+            rawdata = pd.DataFrame(np.array([[model,purchaseyear_value,transmission,mileage_value,fuelType,mpg_value,enginesize_value, make]]), columns=['model','year','transmission','mileage','fuelType','mpg','engineSize','make'])
+            model = loadmodel()
+            result = model.predict(rawdata)
+            output_message = round(float(result))
+            
+        except Exception as e:
+            print (e)
+            raise e 
+    return(str(output_message))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
